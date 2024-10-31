d42693018294540f0fc0783001423d2284261995
nutterthanos <minecraftitsover90@gmail.com>
2024-10-31 12:58:04 +0000

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 # Define file path and increment
-main_script_path = "/home/runner/work/tsto-mayhemids/tsto-mayhemids/grab_mayhemids.py"
+main_script_path = "/home/runner/work/tsto-mayhemids-6/tsto-mayhemids-6/grab_mayhemids.py"
 increment = 1000000
 max_end_id = 6000000000  # define the upper limit for END_ID
 
